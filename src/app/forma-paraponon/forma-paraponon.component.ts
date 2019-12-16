2a5cfb75d2f7f622aa817354f0228844af51bf57
Kostas Tzogka <kwstas_srok@hotmail.com>
2019-12-16 23:52:47 +0200

    added forma-paraponon html/css/js code

--- src/app/forma-paraponon/forma-paraponon.component.ts
+++ src/app/forma-paraponon/forma-paraponon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgbDateStruct, NgbCalendar} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'forma-paraponon',
@@ -7,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormaParapononComponent implements OnInit {
 
-  constructor() { }
+	calendar_date: NgbDateStruct;
+	time = {hour: 0, minute: 0};
+	minDate;
+	maxDate;
+	constructor() {
+		this.minDate = {year: 2010, month: 1, day: 1};
+		let curDate = new Date();
+		this.maxDate = {year:curDate.getFullYear(), month: curDate.getMonth() + 1, day: curDate.getUTCDate() + 1};		
+  }
 
   ngOnInit() {
   }
